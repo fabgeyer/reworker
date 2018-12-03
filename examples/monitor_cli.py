58f763d7f3a1bd0a655b3147223286bf84053804
Fabien Geyer <fgeyer@net.in.tum.de>
2018-12-03 15:53:44 +0100

    Improve monitor CLI example

--- examples/monitor_cli.py
+++ examples/monitor_cli.py
@@ -12,8 +12,16 @@ def monitor():
         while 1:
             i = 0
             stdscr.clear()
+            status = []
             for w in m.list():
-                stdscr.addstr(i, 0, "\r{} {} {}".format(w.key, w.get_ttl(), w.get_status()))
+                status.append((w.key[9:], w.get_ttl(), w.get_status()))
+
+            status = sorted(status, key=lambda x: (x[1], x[0]))
+            for s in status:
+                try:
+                    stdscr.addstr(i, 0, "{}\t{}\t{}".format(*s))
+                except:
+                    break
                 i += 1
             stdscr.refresh()
             time.sleep(1)
